d4530ae37fdc03a27b7b829e8711acb20a0e0768
junyu0813 <junyu0813@gmail.com>
2014-05-24 22:29:41 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -7,5 +7,18 @@
   colnames(testx) <-cname
   colnames(testx) <-cname
   
-# 1) Merge 
+# 1) Merge the training and the test sets to create one data set
+  df <- rbind(trainx, testx)
+
+# 2) Extracts only the measurements on the mean and standard deviation for each measurement.
+# Extract column names which contain "mean()" and "std()"
 df1 <- df[, grepl("mean()", cname, ignore.case=TRUE)|grepl("std()", cname, ignore.case=TRUE)]
+
+# 3) Uses descriptive activity names to name the activities in the data set
+## Load Y_train.txt and Y_test.txt and then merge them
+  trainy <- read.table("D:/R/Y_train.txt", header=F, sep="")
+  testy <- read.table("D:/R/Y_test.txt", header=F, sep="")
+  lname <- rbind(trainy and testy)
+  
+## Load activity label and merge with lname
+  lname <- merge(lname, activity, by = "V1")
